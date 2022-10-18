8e521a2bb90ae89fbf6cb5b3d1e1ad1d1437637b
Khalid Waleed <khalidwaleed0@outlook.com>
2022-10-18 08:05:39 +0200

    Added popup modal

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 class Book {
-    constructor(title, author, isRead) {
-        this.title = title;
-        this.author = author;
-        this.isRead = isRead;
-    }
+	constructor(title, author, isRead) {
+		this.title = title;
+		this.author = author;
+		this.isRead = isRead;
+	}
 }
 let library = [];
 let book1 = new Book("Naruto", "Masashi Kishimoto", true);
 library.push(book1);
 console.log(library);
+
+let modalCard = document.querySelector(".modal-add-book");
+
+document.getElementById("btn-add-book").onclick = () => modalCard.classList.add("active-modal");
+document.querySelector(".modal-add-book").onclick = () => modalCard.classList.remove("active-modal");
+document.querySelector("form").onclick = (e) => e.stopPropagation();
